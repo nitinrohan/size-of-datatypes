53d5d83be04d72a85e30f0f5f5baea1055b6b462
nitinrohan <69236864+nitinrohan@users.noreply.github.com>
2020-10-11 20:36:12 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"the size of char is :"<<sizeof(char)<<"bytes\n";
+	cout<<"the size of int is :"<<sizeof(int)<<"bytes\n";
+	cout<<"the size of float is :"<<sizeof(float)<<"bytes\n";
+	cout<<"the size of double is :"<<sizeof(double)<<"bytes";
+}
